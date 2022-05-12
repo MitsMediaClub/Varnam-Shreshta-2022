5dff92591b24922570f5322b33879f3c0bcc7994
Advaith Narayanan <55801439+Glitchyi@users.noreply.github.com>
2022-05-12 20:35:36 +0530

    nodeing

--- server/https_server.js
+++ server/https_server.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 
 var https_options = {
-    key: fs.readFileSync("public/ssl/infomits.live.key"),
-    cert: fs.readFileSync("public/ssl/infomits.live.cert"),
+    key: fs.readFileSync("./ssl/infomits.live.key"),
+    cert: fs.readFileSync("./ssl/infomits.live.cert"),
     ca: [
     fs.readFileSync('origin'),
     fs.readFileSync('path/to/ca_bundle_certificate.crt')
